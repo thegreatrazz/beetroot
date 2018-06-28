6b820b81d5b29f74c50a0407a1f17c57660bfdd3
Raresh Nistor <me@thegreatrazz.nz>
2018-06-29 09:37:32 +1200

    Import changes I've made

--- app/script.js
+++ app/script.js
@@ -1,3 +1,7 @@
 
+// File system
+var fs = require("fs")
+
+// jQuery is love, jQuery is life
 var jQuery = require("jquery")
 var $ = jQuery
---
+++ build.js
@@ -0,0 +1,34 @@
+
+const fs = require("fs")
+const url = require("url")
+const path = require("path")
+const packager = require("electron-packager")
+
+packager({
+    dir: __dirname,
+    asar: true,
+    executableName: "beetroot-next",
+    name: "Beetroot Player",
+    out: "out",
+    overwrite: true,
+
+    // Target platforms
+    platform: "win32,linux,darwin",
+    arch: "ia32,x64",
+
+    // Windows metadata
+    win32metadata: {
+        CompanyName: "Raresh Nistor (thegreatrazz)",
+        ProductName: "Beetroot",
+        FileDescription: "Media Player with Guest Queueing Capabilities.",
+        InternalName: "beetroot-desktop",
+        "requested-execution-level": "asInvoker"
+    },
+
+    // macOS App Type
+    appCategoryType: "public.app-category.music"
+})
+.then(appPaths =>
+{
+    console.log(appPaths)
+})
